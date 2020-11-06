6f1ceddbd96189085277a1e55303ce79215b5b85
Rafael Monteiro Arjonas <rafaelmonteiroarj@gmail.com>
2020-11-06 14:44:18 -0300

    innitial commit ..

---
+++ README.md
@@ -0,0 +1,19 @@
+# boot-afazenda
+
+Script utilizado para votação dos particiapantes da Fazenda 12.
+
+## Scripts Disponíveis
+
+No diretório do projeto, você pode executar:
+
+### `yarn start:dev`
+
+Executa o aplicativo no modo de desenvolvimento. <br />
+
+### `yarn build`
+
+Gera o build para produção produção. <br />
+
+### `yarn start`
+
+Executa o aplicativo no modo de produção. <br />
---
+++ index.ts
@@ -0,0 +1,49 @@
+import { launch, Page } from "puppeteer";
+import config from 'config';
+
+(async () => {
+  const browser = await launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--window-size=1600,1100"]
+  });
+
+  const click = async (pg: Page, x: string) => {
+    await pg.waitForSelector(x);
+    await pg.click(x);
+    await pg.waitFor(2000);
+  };
+
+  const vote = async (page: Page) => {
+    const elements = await page.$x(
+      "/html/body/div[6]/div[1]/div/div/div/div[1]/ul/li[3]"
+    );
+    await elements[0].click();
+
+    await click(page, ".voting-button");
+    await click(page, ".voting-button");
+    console.log("Voto computado com sucesso!");
+  };
+
+  const votes = 500;
+  const page = await browser.newPage();
+  await page.goto(config.get("App.uri"));
+
+  await click(
+    page,
+    "figure.voting-card:nth-child(3) > div:nth-child(2) > img:nth-child(1)"
+  );
+
+  await click(page, ".voting-button");
+  await click(page, ".voting-button");
+
+  for (let i = 0; i < votes; i++) {
+    await vote(page);
+  }
+
+  page.on("console", async consoleObj => {
+    console.log(consoleObj.text());
+  });
+
+  await browser.close();
+})();
